7f18bda27598ef4d8809982e9fa431e689c55f93
Łukasz Rąbalski <lukaszrabalski@gmail.com>
2023-02-10 09:38:22 +0100

    push

--- posts/draft_multiprocessing/analiza.py
+++ posts/draft_multiprocessing/analiza.py
@@ -1,13 +1,11 @@
-import pandas as pd
-from datetime import datetime
-from time import sleep
-
-def map_function(combo):
-    pd_results_temp=pd.DataFrame(columns=['a','b','c','started'])
-    started = datetime.now()
-    a = combo[0]
-    b = combo[1]
-    c = combo[2]
-    pd_results_temp = pd_results_temp.append({'a':a,'b':b,'c':c,'started':started}, ignore_index=True)
-    sleep(b)
-    return pd_results_temp
+def map_function(args_temp):
+  import pandas as pd
+  from datetime import datetime
+  from time import sleep
+  pd_results_temp=pd.DataFrame(columns=['a','b','started'])
+  started = datetime.now()
+  a = args_temp[0]
+  b = args_temp[1]
+  pd_results_temp = pd_results_temp.append({'a':a,'b':b,'started':started}, ignore_index=True)
+  sleep(b)
+  return pd_results_temp
